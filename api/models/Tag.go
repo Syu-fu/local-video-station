6e3d7f5ed225211c7baa8617f5fd97db4f55421d
Syu fu <syu.fu.dev@gmail.com>
2023-04-04 22:28:57 +0900

    fix: Fix JSON tags naming convention in Tag and Video models

--- api/models/Tag.go
+++ api/models/Tag.go
@@ -5,7 +5,7 @@ import "time"
 type Tag struct {
 	ID          int       `json:"id"`
 	Name        string    `json:"name"`
-	NameReading string    `json:"name_reading"`
+	NameReading string    `json:"nameReading"`
 	CreatedAt   time.Time `json:"-"`
 	UpdatedAt   time.Time `json:"-"`
 }
--- api/models/Video.go
+++ api/models/Video.go
@@ -5,9 +5,9 @@ import "time"
 type Video struct {
 	ID           string    `json:"id"`
 	Title        string    `json:"title"`
-	TitleReading string    `json:"title_reading"`
+	TitleReading string    `json:"titleReading"`
 	Url          string    `json:"url"`
-	ThumbnailUrl string    `json:"thumbnail_url"`
+	ThumbnailUrl string    `json:"thumbnailUrl"`
 	Tags         []Tag     `json:"tags"`
 	CreatedAt    time.Time `json:"-"`
 	UpdatedAt    time.Time `json:"-"`
