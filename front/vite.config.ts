ff80326a8c8be78fdfa40aa8dd20fd8665325207
Syu fu <syu.fu.dev@gmail.com>
2023-04-08 17:57:22 +0900

    feat: Add Jest and Testing Library plugins to ESLint and update TypeScript config to include Vitest globals and setup file, and update Vite config to reference Vitest types

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -15,5 +16,10 @@ export default defineConfig({
   ],
   server: {
     host: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
   },
 });
