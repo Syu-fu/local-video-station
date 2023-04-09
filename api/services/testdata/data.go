28abb45993dbe3eb5cbef596cccb794610836e1f
Syu fu <syu.fu.dev@gmail.com>
2023-04-09 22:26:21 +0900

    test: Add test for getting a video by ID

--- api/services/testdata/data.go
+++ api/services/testdata/data.go
@@ -4,11 +4,23 @@ import "api/models"
 
 var VideoTestData = []models.Video{
 	{
-		ID:           "12345-67890-12345-67890",
+		ID:           "3C6FC606-3DC8-4EE5-AFC0-D6C20DE47AE4",
 		Title:        "How to Make Sushi",
 		TitleReading: "How to Make Sushi",
 		Url:          "https://example.com/how-to-make-sushi",
 		ThumbnailUrl: "https://example.com/sushi-thumbnail.jpg",
+		Tags: []models.Tag{
+			{
+				ID:          "74FF4BBA-18A5-4ABC-9164-4987021D411F",
+				Name:        "Sushi",
+				NameReading: "Sushi",
+			},
+			{
+				ID:          "A082A0B6-63DE-4E97-BA30-B29319863D4F",
+				Name:        "Cooking",
+				NameReading: "Cooking",
+			},
+		},
 	},
 	{
 		ID:           "09876-54321-09876-54321",
--- api/services/video_service_test.go
+++ api/services/video_service_test.go
@@ -3,6 +3,7 @@ package services_test
 import (
 	"testing"
 
+	"api/models"
 	"api/services/testdata"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -32,4 +33,54 @@ func TestGetVideoCountService(t *testing.T) {
 	if num := got; num != expectedNum {
 		t.Errorf("want %d but got %d videos\n", expectedNum, num)
 	}
+}
+
+func TestGetVideoService(t *testing.T) {
+	got, err := aSer.GetVideoService(testdata.VideoTestData[0].ID)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if !equalVideo(testdata.VideoTestData[0], got) {
+		t.Errorf("want %v(%T) but got %v(%T)\n", testdata.VideoTestData[0], testdata.VideoTestData[0], got, got)
+	}
+}
+
+func equalVideo(v1, v2 models.Video) bool {
+	if v1.ID != v2.ID {
+		return false
+	}
+
+	if v1.Title != v2.Title {
+		return false
+	}
+
+	if v1.TitleReading != v2.TitleReading {
+		return false
+	}
+
+	if v1.Url != v2.Url {
+		return false
+	}
+
+	if v1.ThumbnailUrl != v2.ThumbnailUrl {
+		return false
+	}
+
+	if len(v1.Tags) != len(v2.Tags) {
+		return false
+	}
+
+	tagMap1 := make(map[models.Tag]bool)
+	for _, tag := range v1.Tags {
+		tagMap1[tag] = true
+	}
+
+	for _, tag := range v2.Tags {
+		if _, ok := tagMap1[tag]; !ok {
+			return false
+		}
+	}
+
+	return true
 }
