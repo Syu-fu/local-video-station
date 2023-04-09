6a84da0e758c66ea51c7acec311a2760afda7692
Syu fu <syu.fu.dev@gmail.com>
2023-04-09 22:31:51 +0900

    test: Add unit tests for VideoDetailHandler

--- api/controllers/testdata/mock.go
+++ api/controllers/testdata/mock.go
@@ -1,6 +1,9 @@
 package testdata
 
-import "api/models"
+import (
+	"api/apperrors"
+	"api/models"
+)
 
 type serviceMock struct{}
 
@@ -16,4 +19,15 @@ func (s *serviceMock) GetVideoCountService() (int, error) {
 	videoCount := 2
 
 	return videoCount, nil
+}
+
+func (s *serviceMock) GetVideoService(id string) (models.Video, error) {
+	if id == "3C6FC606-3DC8-4EE5-AFC0-D6C20DE47AE4" {
+		return VideoTestData[0], nil
+	}
+
+	var err error
+	err = apperrors.NAData.Wrap(err, "no data")
+
+	return models.Video{}, err
 }
