7008ebe7afbcf3182f99e304a5760d7ae041d512
Syu fu <syu.fu.dev@gmail.com>
2023-04-09 22:52:21 +0900

    test: Add test link in VideoListItem.test.tsx

--- front/src/components/__tests__/VideoListItem.test.tsx
+++ front/src/components/__tests__/VideoListItem.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import type Video from '../../types/video';
 import VideoListItemComponent from '../VideoListItemComponent';
 
@@ -10,10 +11,15 @@ describe('VideoListItem', () => {
     titleReading: 'How to Make Sushi',
     url: 'http://test.com/movie.mp4',
     thumbnailUrl: testThumbnailUrl,
+    tags: [],
   };
 
   it('should render video thumbnail, title, and url', () => {
-    render(<VideoListItemComponent video={video} />);
+    render(
+      <MemoryRouter>
+        <VideoListItemComponent video={video} />
+      </MemoryRouter>
+    );
     const cardMediaElement = screen.getByRole('img');
     const title = screen.getByText(video.title);
 
@@ -22,4 +28,30 @@ describe('VideoListItem', () => {
       `background-image: url(${testThumbnailUrl});`
     );
   });
+});
+
+test('renders video title and link', () => {
+  const video: Video = {
+    id: '3C6FC606-3DC8-4EE5-AFC0-D6C20DE47AE4',
+    title: 'How to Make Sushi',
+    titleReading: 'How to Make Sushi',
+    url: 'http://test.com/movie.mp4',
+    thumbnailUrl: 'http://test.com/thumbnail.jpg',
+    tags: [],
+  };
+
+  render(
+    <MemoryRouter>
+      <VideoListItemComponent video={video} />
+    </MemoryRouter>
+  );
+
+  const linkElement = screen.getByRole('link');
+  expect(linkElement).toHaveAttribute(
+    'href',
+    '/video/3C6FC606-3DC8-4EE5-AFC0-D6C20DE47AE4'
+  );
+
+  const titleElement = screen.getByText('How to Make Sushi');
+  expect(titleElement).toBeInTheDocument();
 });
