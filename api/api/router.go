600e861954e8c80808fff3f6418991b387595924
Syu fu <syu.fu.dev@gmail.com>
2023-04-13 23:15:55 +0900

    feat: Add POST endpoint for creating tags in TagController in NewRouter

--- api/api/router.go
+++ api/api/router.go
@@ -13,11 +13,14 @@ import (
 func NewRouter(db *sql.DB) *echo.Echo {
 	ser := services.NewMyAppService(db)
 	aCon := controllers.NewVideoController(ser)
+	tCon := controllers.NewTagController(ser)
 	e := echo.New()
 
 	e.GET("/video/list", aCon.VideoListHandler)
 	e.GET("/video/count", aCon.VideoCountHandler)
 	e.GET("/video/:id", aCon.VideoDetailHandler)
+
+	e.POST("/tag", tCon.PostTagHandler)
 
 	e.Use(middleware.CORS())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
