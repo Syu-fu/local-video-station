ae6c96fc85d93ed53e2a5c8e4977040672a636e7
Syu fu <syu.fu.dev@gmail.com>
2023-04-19 22:16:15 +0900

    feat: Add functionality to get a list of videos by tags

--- api/services/video_service.go
+++ api/services/video_service.go
@@ -123,3 +123,20 @@ func (s *MyAppService) PostVideoService(video models.Video, thumbnailFile io.Rea
 
 	return newVideo, nil
 }
+
+func (s *MyAppService) GetVideoListByTagsService(tagIDs string, page int) ([]models.Video, error) {
+	videoList, err := repositories.SelectVideoListByTags(s.db, tagIDs, page)
+	if err != nil {
+		err = apperrors.GetDataFailed.Wrap(err, "fail to get data")
+
+		return nil, err
+	}
+
+	if len(videoList) == 0 {
+		err := apperrors.NAData.Wrap(ErrNoData, "no data")
+
+		return nil, err
+	}
+
+	return videoList, nil
+}
