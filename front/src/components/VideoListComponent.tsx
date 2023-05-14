0914dda0b9a496e1cbd16c515fe472997b125220
Syu fu <syu.fu.dev@gmail.com>
2023-05-14 17:48:19 +0900

    fix: Update VideoListComponent styling

--- front/src/components/VideoListComponent.tsx
+++ front/src/components/VideoListComponent.tsx
@@ -1,5 +1,5 @@
 import { type FC } from 'react';
-import { Box } from '@mui/material';
+import { Grid } from '@mui/material';
 import type Video from '../types/video';
 import VideoListItemComponent from './VideoListItemComponent';
 
@@ -9,34 +9,13 @@ interface Props {
 
 const VideoListComponent: FC<Props> = ({ videos }) => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        boxSizing: 'border-box',
-        width: '100%',
-
-        paddingRight: '8px',
-        overflowX: 'hidden',
-      }}
-    >
+    <Grid container spacing={2}>
       {videos.map((video) => (
-        <Box
-          key={video.id}
-          sx={{
-            flexGrow: 1,
-            flexBasis: { xs: '100%', sm: 'calc(50% - 8px)' },
-            minWidth: { sm: '300px' },
-            boxSizing: 'border-box',
-            paddingLeft: '4px',
-            paddingRight: '4px',
-            marginBottom: '16px',
-          }}
-        >
+        <Grid item xs={12} sm={6} key={video.id}>
           <VideoListItemComponent video={video} />
-        </Box>
+        </Grid>
       ))}
-    </Box>
+    </Grid>
   );
 };
 
